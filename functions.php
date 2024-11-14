73494074e4a2caefacbacfb9ee924cc36db481d4
Cristian Cunanan <cunanancristian5@gmail.com>
2024-11-14 19:33:57 +0800

    Database Connection Added

--- functions.php
+++ functions.php
@@ -1,3 +1,17 @@
 <?php    
-    // All project functions should be placed here
+    // Database Connection
+function db_connect() {
+    $host = 'localhost';
+    $user = 'root';
+    $password = ''; // Default for Laragon
+    $database = 'dct-ccs-finals';
+
+    $connection = new mysqli($host, $user, $password, $database);
+
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+    }
+
+    return $connection;
+}
 ?>
